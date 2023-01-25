ca3bec18217831156211ca56a463855b28c1ae53
Tom Snelling <tomsnelling8@gmail.com>
2023-01-25 17:46:45 +0000

    adds basic tests

--- test/src/App.js
+++ test/src/App.js
@@ -1,9 +1,13 @@
+import { useRef } from "react";
 import { useHermes, useHermesState } from "@hermes/react";
 
 const App = () => {
   return (
     <div id="hermes-app">
+      <h1>Hermes demo</h1>
       <HermesState />
+      <AllUsers />
+      <FilteredUsers />
     </div>
   );
 };
@@ -13,7 +17,67 @@ const HermesState = () => {
 
   return (
     <div>
-      <p data-testid="connected">{JSON.stringify(connected)}</p>
+      <p>
+        connected:{" "}
+        <span data-testid="connected">{JSON.stringify(connected)}</span>
+      </p>
+    </div>
+  );
+};
+
+const RenderUsers = ({ users }) => (
+  <>
+    {users.map((user) => (
+      <div style={{ border: "1px solid black", padding: "0px 16px" }}>
+        {Object.entries(user).map(([k, v]) => (
+          <p>
+            {k}:{" "}
+            <span className={k}>
+              {typeof v === "string" ? v : JSON.stringify(v)}
+            </span>
+          </p>
+        ))}
+      </div>
+    ))}
+  </>
+);
+
+const AllUsers = () => {
+  const users = useHermes("test_users");
+
+  const renderCount = useRef(0);
+  renderCount.current = renderCount.current + 1;
+
+  return (
+    <div style={{ border: "1px solid black", padding: "0px 16px" }}>
+      <p>AllUsers</p>
+      <p>
+        renders:{" "}
+        <span data-testid="allusers-renders">{renderCount.current}</span>
+      </p>
+      <div data-testid="allusers-documents">
+        <RenderUsers users={users} />
+      </div>
+    </div>
+  );
+};
+
+const FilteredUsers = () => {
+  const users = useHermes("test_users", [{ $match: { username: "bob" } }]);
+
+  const renderCount = useRef(0);
+  renderCount.current = renderCount.current + 1;
+
+  return (
+    <div style={{ border: "1px solid black", padding: "0px 16px" }}>
+      <p>AllUsers</p>
+      <p>
+        renders:{" "}
+        <span data-testid="filteredusers-renders">{renderCount.current}</span>
+      </p>
+      <div data-testid="filteredusers-documents">
+        <RenderUsers users={users} />
+      </div>
     </div>
   );
 };
--- test/src/test.ts
+++ test/src/test.ts
@@ -1,10 +1,14 @@
 import hermes from "@hermes/server";
 import { test, expect, Page } from "@playwright/test";
+import { MongoClient, Collection } from "mongodb";
 import { config } from "dotenv";
 
 config();
 
+const wait = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 let page: Page;
+let collection: Collection;
 
 test.describe.configure({ mode: "serial" });
 
@@ -14,22 +18,101 @@ test.describe("hermes", () => {
       srv: process.env.MONGO_SRV,
       db: process.env.MONGO_DB,
       whitelist: {
-        users: ["username", "name.first", "name.last"],
+        test_users: ["username", "name.first", "name.last"],
       },
     });
 
     page = await browser.newPage();
+
+    const client = new MongoClient(process.env.MONGO_SRV);
+    await client.connect();
+    collection = client.db(process.env.MONGO_DB).collection("test_users");
+    await collection.deleteMany({});
   });
 
   test.afterAll(async () => {
     await page.close();
   });
 
-  test("connection succeeds", async ({ page, baseURL }) => {
+  test("connection succeeds", async ({ baseURL }) => {
     await page.goto(baseURL);
     await page.waitForSelector("#hermes-app");
 
     const text = await page.getByTestId("connected");
     await expect(text).toContainText("true");
+  });
+
+  test("hook is correctly updated on insert", async () => {
+    await collection.insertOne({
+      username: "alice",
+      name: { first: "Alice", middle: "B", last: "Z" },
+      password: "LoremIpsum",
+    });
+
+    await wait(500);
+
+    const container = await page.getByTestId("allusers-documents");
+    const users = await container.locator("> div");
+    await expect(users).toHaveCount(1);
+
+    const firstUserAttributes = await users.first().locator("> p");
+    await expect(firstUserAttributes).toHaveCount(3);
+
+    const firstUserId = await firstUserAttributes.locator("._id");
+    await expect(await firstUserId.isVisible()).toBe(true);
+
+    const firstUserUsername = await firstUserAttributes.locator(".username");
+    await expect(firstUserUsername).toHaveText("alice");
+
+    const firstUserName = await firstUserAttributes.locator(".name");
+    await expect(firstUserName).toHaveText('{"first":"Alice","last":"Z"}');
+
+    const firstUserPassword = await firstUserAttributes.locator(".password");
+    await expect(await firstUserPassword.isVisible()).toBe(false);
+  });
+
+  test("hook with query does not contain non-matching document", async () => {
+    const container = await page.getByTestId("filteredusers-documents");
+    const users = await container.locator("> div");
+    await expect(users).toHaveCount(0);
+  });
+
+  test("hook is correctly updated on update", async () => {
+    await collection.findOneAndUpdate(
+      { username: "alice" },
+      { $set: { "name.last": "Y" } }
+    );
+
+    await wait(500);
+
+    const container = await page.getByTestId("allusers-documents");
+    const users = await container.locator("> div");
+    await expect(users).toHaveCount(1);
+
+    const firstUserAttributes = await users.first().locator("> p");
+    await expect(firstUserAttributes).toHaveCount(3);
+
+    const firstUserName = await firstUserAttributes.locator(".name");
+    await expect(firstUserName).toHaveText('{"first":"Alice","last":"Y"}');
+
+    await collection.findOneAndUpdate(
+      { username: "alice" },
+      { $set: { "name.middle": "C" } }
+    );
+
+    await wait(500);
+
+    const firstUserName2 = await firstUserAttributes.locator(".name");
+    await expect(firstUserName2).toHaveText('{"first":"Alice","last":"Y"}');
+  });
+
+  test("hook is correctly updated on delete", async () => {
+    await collection.deleteOne({ username: "alice" });
+
+    await wait(500);
+
+    const container = await page.getByTestId("allusers-documents");
+    const users = await container.locator("> div");
+    await expect(users).toHaveCount(0);
   });
 });
